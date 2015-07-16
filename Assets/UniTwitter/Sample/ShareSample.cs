b8143f8dd66567add400f0d868a3fb5d45ad9658
sanuki.wataru <ericsson.goodjob@gmail.com>
2015-07-16 13:07:02 +0900

    FirstCommit

---
+++ Assets/UniTwitter/Sample/ShareSample.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShareSample : MonoBehaviour {
+
+    public void Share(){
+        UniTwitter.Share ("Share your game info!");
+    }
+}
